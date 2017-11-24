7ba157c4274ae23e041ee540296e823b06695311
Im Khem <im.khem@gmail.com>
2017-11-24 14:36:43 -0800

    chapter 7 section 3

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,136 @@
-/* GET 'home' page */
-module.exports.homelist = function(req, res){
-  res.render('locations-list', { title: 'Home' });
+var request = require('request');
+var apiOptions = { server: "http://localhost:3000" };
+
+if(process.env.NODE_ENV === 'production'){
+  apiOptions.server = "something else";
+}
+
+var renderHomepage = (req, res, responseBody)=>{
+  var message;
+
+  if(!(responseBody instanceof Array)) {
+    message = "API lookup error";
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = "No places found nearby";
+    }
+  }
+
+  res.render('locations-list', {
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+        title: 'Loc8r',
+        strapline: 'Find places to work with wifi near you!'
+    },
+    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+    locations: responseBody,
+    message: message
+  });
 };
 
+/* GET 'home' page */
+module.exports.homelist = function(req, res) {
+  var requestOptions, path;
+  path = '/api/locations';
+
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: "GET",
+    json: {},
+    qs: {
+      lng: -119.6984880,
+      lat: 36.7345920,
+      maxDistance: 60
+    }
+  };
+
+  request (requestOptions, (err, response, body)=>{
+    var i, data,
+    data = body;
+    
+    if(response.statusCode === 200 && data.length){
+      for(i=0; i<data.length; i++){
+        data[i].distance = _formatDistance(data[i].distance);
+      }
+    }
+    renderHomepage(req, res, body);
+  })
+};
+
+var _formatDistance = (distance)=>{
+  var numDistance, unit;
+  if(distance > 1) {
+    numDistance = parseFloat(distance).toFixed(1);
+    unit = 'km';
+  } else {
+    numDistance = parseInt(distance * 1000,10);
+    unit = 'm';
+  }
+  return numDistance + unit;
+}
+
+var renderDetailPage = (req, res, locDetail)=>{
+  res.render('location-info', {
+    title: locDetail.name,
+    pageHeader: {
+        title: locDetail.name
+    },
+    sidebar: {
+        context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+        callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location: locDetail
+  });
+}
+
 /* GET 'Location info' page */
-module.exports.locationInfo = function(req, res){
-  res.render('location-info', { title: 'Location info' });
+module.exports.locationInfo = function(req, res) {
+  var requestOptions, path;
+  path = "/api/locations/" + req.params.locationid;
+
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: "GET",
+    json: {}
+  };
+
+  request(requestOptions, (err, response, body)=>{
+    var data = body;
+    if(response.statusCode === 200) {
+      data.coords = {
+        lng: body.coords[0],
+        lat: body.coords[1]
+      };
+      renderDetailPage(req,res, data);
+    } else {
+      _showError(req, res, response.statusCode);
+    }
+  })
+};
+
+var _showError = (req, res, status)=>{
+  var title, content;
+  if(status === 404) {
+    title = "404, page not found";
+    content = "Oh dear. Looks like we can't find this page. Sorry.";
+  } else {
+    title = status + ", something's gone wrong";
+    content = "Something, somewhere, has gone just a little bit wrong.";
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title: title,
+    content: content
+  });
 };
 
 /* GET 'Add review' page */
-module.exports.addReview = function(req, res){
-  res.render('location-review-form', { title: 'Add review' });
+module.exports.addReview = function(req, res) {
+  res.render('location-review-form', {
+      title: 'Review Starcups on Loc8r',
+      pageHeader: {
+          title: 'Review Starcups'
+      }
+  });
 };
